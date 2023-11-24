50919171fc9db213f16af657d80ff44b5d3974b9
Mye270419 <127803889+Mye270419@users.noreply.github.com>
2023-11-24 08:27:10 -0400

    Add files via upload

---
+++ paginaboostrap/images/Quienes_somos.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="quienes.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!--header-->
+    <div class="container">
+        <?php include('php/header.php'); ?>
+
+
+        <div class="container">
+
+            <h1 class="titulo">Grupo Global Juridico</h1>
+            <br>
+            <h2>Quiénes Somos</h2>
+            <br>
+            <img src="images/abogados.jpg" alt="" width="350px">
+            <p>Somos un equipo de abogados altamente calificados con años de experiencia en una amplia gama de áreas
+                legales. Nuestra misión es proporcionar asesoramiento legal excepcional y representación efectiva para
+                nuestros clientes.</p>
+            <img src="images/manos.jpg" alt=" " width="400px">
+            <p>Nuestro compromiso es con la justicia y la protección de los derechos de nuestros clientes. Nos
+                enorgullece ofrecer servicios legales de alta calidad y un trato personalizado a cada cliente.</p>
+            <br>
+            <p>En el Grupo Global Juridico, nos especializamos en diversas áreas del derecho, incluyendo:</p>
+            <div class="lista">
+                <ul>
+                    <li>Derecho Civil</li>
+                    <li>Derecho Penal</li>
+                    <li>Derecho Laboral</li>
+                    <li>Derecho de Familia</li>
+                    <li>Derecho de Inmigración</li>
+                </ul>
+            </div>
+            <img src="images/civil.jpg"
+                alt=""><!--puedo ponerlo con float left para que todo se alinee pero ne se ve lijndo una galeria con grid tal ves pero oxcupa mucho espacio-->
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <br> <br><br>
+            <p>Nuestro equipo se dedica a escuchar a nuestros clientes, comprender sus necesidades y brindar soluciones
+                legales efectivas. Estamos comprometidos a trabajar incansablemente en su nombre, ya sea en un caso
+                complejo en el tribunal o en la negociación de un acuerdo legal.</p>
+
+            <p>Confíe en nosotros para resolver sus problemas legales de manera eficaz y ética. Estamos aquí para
+                ayudarle en cada paso del camino.</p>
+        </div>
+
+
+        <?php include('php/footer.php'); ?>
+
+
+</body>
+
+</html>
---
+++ paginaboostrap/php/footer.php
@@ -0,0 +1,22 @@
+<section class="footer">
+    <footer>
+      <div class="footerContainer">
+        <div class="socialIcons">
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-youtube"></i></a>
+          <a href="#"><i class="fa-brands fa-twitter"></i></a>
+          <a href="#"><i class="fa-brands fa-telegram"></i></a>
+          <a href="#"><i class="fa-brands fa-reddit-alien"></i></a>
+        </div>
+        <div class="footerNav">
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Contactanos</a></li>
+            <li><a href="3">Nuestro equipo</a></li>
+          </ul>
+        </div>
+
+
+    </footer>
+  </section>
